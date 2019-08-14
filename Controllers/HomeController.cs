cf5334d827f4c875ba1d45d4002e709bc8ec2954
Steven <steven.chung96@yahoo.com>
2019-08-13 20:19:48 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using quoting_dojo.Models;
+
+namespace quoting_dojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Dictionary<string, object>> AllUsers
+        {
+            get
+            {
+                return DbConnector.Query("SELECT * FROM users;");
+            }
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(string name, string quote)
+        {
+            System.Console.WriteLine("hello?");
+
+            string insert = $"INSERT INTO quotes (name, quote) VALUES ('{name}', '{quote}');";
+            DbConnector.Execute(insert);
+            return RedirectToAction("Quotes");
+        }
+
+        [HttpGet("result")]
+        public IActionResult Quotes()
+        {
+            List<Dictionary<string, object>> AllQuotes = DbConnector.Query("SELECT * FROM quotes ORDER BY name desc");
+
+            // To provide this data, we could use ViewBag or a View Model.  ViewBag shown here:
+            ViewBag.Quotes = AllQuotes;
+                return View("Result");
+        }
+
+        
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace quoting_dojo
+{
+    public class User
+    {
+        public int id {get;set;}
+        public string nam {get;set;}
+        public string quote {get;set;}
+        public DateTime created_at {get;set;}
+        public DateTime updated_at {get;set;}
+    }
+
+}
